7a3c73ebca307b331946ff277547b1a29112e3f8
Liam-McCormack <liammc12@hotmail.co.uk>
2020-04-21 14:23:15 +0100

    First Codecademy projects

---
+++ Dog Years Challenge/home.js
@@ -0,0 +1,9 @@
+
+function dogAge(dogYears) { 
+    if (dogYears <= 2) 
+    humanAge = dogYears * 10.5
+    else humanAge = (dogYears - 2) * 4 + 21
+    console.log(`A ${dogYears} year old dog, is ${humanAge} years old in human years`); 
+}
+
+dogAge(3);
---
+++ Magic Eight Ball/magiceightball.js
@@ -0,0 +1,24 @@
+console.log("Will I be rich?");
+
+let randomNumber = Math.floor(Math.random() * 8) + 1
+let magicBallAnswer = "";
+
+if (randomNumber === 1) {
+    magicBallAnswer = 'No chance';
+} else if (randomNumber === 2) {
+    magicBallAnswer = 'Yes';
+} else if (randomNumber === 3) {
+    magicBallAnswer = 'No';
+} else if (randomNumber === 4) {
+    magicBallAnswer = 'Maybe in the future';
+} else if (randomNumber === 5) {
+    magicBallAnswer = 'VSooner than expected';
+} else if (randomNumber === 6) {
+    magicBallAnswer = 'Maybe';
+} else if (randomNumber === 7) {
+    magicBallAnswer = 'Definitely maybe';
+} else if (randomNumber === 8) {
+    magicBallAnswer = 'Who knows';
+}
+
+console.log(magicBallAnswer);
